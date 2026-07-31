af8a808b943a6db65070b5733b387ae2058d1bbd
Kayke Andrade <kaykega87@gmail.com>
2026-07-31 12:30:31 -0300

    feat(finance): adicionar model Trabalhador

--- lagoagro/finance/admin.py
+++ lagoagro/finance/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import LancamentoFinanceiro
+from .models import LancamentoFinanceiro, Trabalhador
 
 admin.site.register(LancamentoFinanceiro)
+admin.site.register(Trabalhador)
