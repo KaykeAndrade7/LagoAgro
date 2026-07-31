c8e84f15e12df725f01dc0bbd289ffebdefc893e
Kayke Andrade <kaykega87@gmail.com>
2026-07-30 23:30:56 -0300

    fix(properties): remover coercao desnecessaria de Decimal, corrigir teste

--- lagoagro/properties/models.py
+++ lagoagro/properties/models.py
@@ -1,5 +1,3 @@
-from decimal import Decimal
-
 from django.conf import settings
 from django.db import models
 
@@ -14,20 +12,11 @@ class Propriedade(models.Model):
         return self.nome
 
 
-class TalhaoManager(models.Manager):
-    def create(self, **kwargs):
-        if 'area' in kwargs:
-            kwargs['area'] = Decimal(str(kwargs['area']))
-        return super().create(**kwargs)
-
-
 class Talhao(models.Model):
     propriedade = models.ForeignKey(Propriedade, on_delete=models.CASCADE, related_name="talhoes")
     nome = models.CharField(max_length=100)
     area = models.DecimalField(max_digits=10, decimal_places=2)  # hectares
     tipo_solo = models.CharField(max_length=100)
-
-    objects = TalhaoManager()
 
     def __str__(self):
         return f"{self.nome} ({self.propriedade.nome})"
