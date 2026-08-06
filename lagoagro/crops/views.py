4b9f8ddd970fd058df9eb4827d7e61ea0eddc00a
Kayke Andrade <kaykega87@gmail.com>
2026-08-06 14:38:21 -0300

    feat(crops): abrir CRUD de Cultura com escopo por conta e fases aninhadas

--- lagoagro/crops/views.py
+++ lagoagro/crops/views.py
@@ -1,9 +1,25 @@
+from django.db.models import Q
 from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
 
 from .models import Cultura
 from .serializers import CulturaSerializer
 
 
-class CulturaViewSet(viewsets.ReadOnlyModelViewSet):
+class CulturaViewSet(viewsets.ModelViewSet):
     queryset = Cultura.objects.all()
     serializer_class = CulturaSerializer
+
+    def get_queryset(self):
+        # Uniao: catalogo embutido (usuario nulo) + culturas da propria
+        # conta. Nunca vaza cultura de outra conta.
+        return Cultura.objects.filter(Q(usuario__isnull=True) | Q(usuario=self.request.user)).order_by("nome")
+
+    def get_object(self):
+        obj = super().get_object()
+        if self.action in ("update", "partial_update", "destroy") and obj.usuario_id is None:
+            raise PermissionDenied("Não é possível editar ou excluir uma cultura do catálogo padrão.")
+        return obj
+
+    def perform_create(self, serializer):
+        serializer.save(usuario=self.request.user)
